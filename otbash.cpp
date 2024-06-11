6d04df5ec6d30c5101f9306b393e5f22ae4ff06c
Arduinix12 <151068595+Arduinix12@users.noreply.github.com>
2024-06-11 09:19:57 +0300

    Update otbash.cpp

--- otbash.cpp
+++ otbash.cpp
@@ -6,40 +6,42 @@ using namespace std;
 
 
 string Otbash::encryptOtbas(string text) {
-    
+    string encryptedText = "";
 
-    for (int i = 0; i < text.length(); i++) {
-        if (isalpha(text[i])) {
-            if (isupper(text[i])) {
-                text += char('Z' - (text[i] - 'A'));
+
+        for (int i = 0; i < text.length(); i++) {
+            if (isalpha(text[i])) {
+                if (isupper(text[i])) {
+                    encryptedText += char('Z' - (text[i] - 'A'));
+                }
+                else {
+                    encryptedText += char('z' - (text[i] - 'a'));
+                }
             }
             else {
-                text += char('z' - (text[i] - 'a'));
+                encryptedText += text[i];
             }
         }
-        else {
-            text += text[i];
-        }
-    }
-    return text;
+    return encryptedText;
 }
 
 string Otbash::decryptOtbas(string text) {
-    
+    string decryptedText = "";
 
-    for (char ch : text) {
-        if (isalpha(ch)) {
-            if (islower(ch)) {
-                text += 'z' - (ch - 'a');
+
+        for (char ch : text) {
+            if (isalpha(ch)) {
+                if (islower(ch)) {
+                    decryptedText += 'a' + ('z' - ch);
+                }
+                else {
+                    decryptedText += 'A' + ('Z' - ch);
+                }
             }
             else {
-                text += 'Z' - (ch - 'A');
+                decryptedText += ch;
             }
         }
-        else {
-            text += ch;
-        }
-    }
 
-    return text;
+    return decryptedText;
 }
