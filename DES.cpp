915519171aacc36c98234164cff51c08b2809081
Arduinix12 <151068595+Arduinix12@users.noreply.github.com>
2024-07-25 11:15:46 +0300

    Update DES.cpp

--- DES.cpp
+++ DES.cpp
@@ -462,6 +462,80 @@ void DES::getKeys() {
     get_C_D(a);
     getKeyI();
 }
+bool DES::deleteFile(string filePath)
+{
+    if (remove(filePath.c_str()) != 0) {
+        cerr << "Error deleting file: " << filePath << "\n";
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+int DES::chekfile()
+{
+    switch (mode)
+    {
+    case 1:
+
+        if (finputfile.is_open()) {
+
+            return 0;
+        }
+        else
+        {
+            cerr << "error opened file\n";
+            return 1;
+        }
+    case 2:
+
+        if (finputfile.is_open()) {
+            if (foutputfile.is_open()) {
+
+                return 0;
+            }
+            else
+            {
+                cerr << "error opened file\n";
+                return 2;
+            }
+
+        }
+        else
+        {
+            cerr << "error opened file\n";
+            return 1;
+        }
+    case 3:
+        if (finputfile.is_open()) {
+            if (foutputfile.is_open()) {
+
+                if (fkeyfile.is_open()) {
+
+                    return 0;
+
+                }
+                else
+                {
+                    cerr << "error opened file\n";
+                    return 3;
+                }
+            }
+            else
+            {
+                cerr << "error opened file\n";
+                return 2;
+            }
+
+        }
+        else
+        {
+            cerr << "error opened file\n";
+            return 1;
+        }
+
+    }
+}
 int DES::chekkey(string key) {
 
     int longs = key.length();
@@ -492,4 +566,143 @@ int DES::chekstring(string str) {
         return -1;
     }
 
+}
+
+DES::DES()
+{
+    int mode = 0;
+    this->mode = mode;
+
+}
+
+DES::DES(string inputfile)
+{
+    int mode = 1;
+    this->mode = mode;
+
+    this->inputfile = inputfile;
+    finputfile.open(inputfile, fstream::in | fstream::out | fstream::app);
+
+    chekfile();
+}
+
+DES::DES(string inputfile, string outputfile)
+{
+    int mode = 2;
+    this->mode = mode;
+
+    this->inputfile = inputfile;
+    finputfile.open(inputfile, fstream::in | fstream::out | fstream::app);
+    this->outputfile = outputfile;
+    finputfile.open(outputfile, fstream::in | fstream::out | fstream::app);
+    chekfile();
+}
+
+DES::DES(string inputfile, string outputfile, string keyfile)
+{
+    int mode = 3;
+    this->mode = mode;
+
+    this->inputfile = inputfile;
+    finputfile.open(inputfile, fstream::in | fstream::out | fstream::app);
+    this->outputfile = outputfile;
+    finputfile.open(outputfile, fstream::in | fstream::out | fstream::app);
+    this->keyfile = keyfile;
+    fkeyfile.open(keyfile, fstream::in | fstream::out | fstream::app);
+    chekfile();
+}
+
+DES::~DES()
+{
+    switch (mode) {
+    case 1:
+        finputfile.close();
+    case 2:
+        finputfile.close();
+        foutputfile.close();
+    case 3:
+        finputfile.close();
+        foutputfile.close();
+        fkeyfile.close();
+
+
+    }
+
+}
+
+int DES::read_Datafile(int cryptomode, string key)
+{
+    switch (mode)
+    {
+    case 1:
+        if (cryptomode == 0) {
+            std::vector<char> buffer(std::istreambuf_iterator<char>(finputfile), (std::istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            encode(str, key);
+            finputfile.close();
+            deleteFile(inputfile);
+            finputfile.open(inputfile, fstream::in | fstream::out | fstream::app);
+            finputfile << str;
+            return 0;
+        }
+        else if (cryptomode == 1) {
+            std::vector<char> buffer(std::istreambuf_iterator<char>(finputfile), (std::istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            decode(str, key);
+            finputfile.close();
+            deleteFile(inputfile);
+            finputfile.open(inputfile, fstream::in | fstream::out | fstream::app);
+            finputfile << str;
+            return 0;
+        }
+        break;
+    case 2:
+        if (cryptomode == 0) {
+            std::vector<char> buffer(std::istreambuf_iterator<char>(finputfile), (std::istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            encode(str, key);
+            foutputfile << str;
+
+        }
+        else if (cryptomode == 1) {
+            std::vector<char> buffer(std::istreambuf_iterator<char>(finputfile), (std::istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            decode(str, key);
+            finputfile << str;
+        }
+        break;
+    case 3:
+        cerr << "ошибка слишком много аргументов в функции!\n";
+        break;
+    }
+}
+
+int DES::read_Datafile(int cryptomode)
+{
+    if (mode == 3) {
+        if (cryptomode == 0) {
+            vector<char> buffer(istreambuf_iterator<char>(finputfile), (istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            vector<char> keybuffer(istreambuf_iterator<char>(fkeyfile), (istreambuf_iterator<char>()));
+            string keystr(keybuffer.begin(), keybuffer.end());
+            encode(str, keystr);
+            foutputfile << str;
+            return 0;
+        }
+        else if (cryptomode == 1) {
+            vector<char> buffer(istreambuf_iterator<char>(finputfile), (istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            vector<char> keybuffer(istreambuf_iterator<char>(fkeyfile), (istreambuf_iterator<char>()));
+            string keystr(keybuffer.begin(), keybuffer.end());
+            decode(str, keystr);
+            foutputfile << str;
+            return 0;
+        }
+    }
+    else {
+
+
+        cerr << "ошибка слишком мало аргументов в функции!\n";
+        return 1;
+    }
 }
