c7b4b5ea71626ebbdd788ca9da48b13423c552b3
Arduinix12 <151068595+Arduinix12@users.noreply.github.com>
2024-07-25 11:32:34 +0300

    Update vigenere.cpp

--- vigenere.cpp
+++ vigenere.cpp
@@ -2,6 +2,50 @@
 
 using namespace std;
 
+VigenereCipher::VigenereCipher()
+{
+    int mode = 0;
+    this->mode = mode;
+
+}
+
+VigenereCipher::VigenereCipher(string inputfile)
+{
+    int mode = 1;
+    this->mode = mode;
+
+    this->inputfile = inputfile;
+    finputfile.open(inputfile, fstream::in | fstream::out | fstream::app);
+
+    chekfile();
+}
+
+VigenereCipher::VigenereCipher(string inputfile, string outputfile)
+{
+    int mode = 2;
+    this->mode = mode;
+
+    this->inputfile = inputfile;
+    finputfile.open(inputfile, fstream::in | fstream::out | fstream::app);
+    this->outputfile = outputfile;
+    finputfile.open(outputfile, fstream::in | fstream::out | fstream::app);
+    chekfile();
+}
+
+VigenereCipher::VigenereCipher(string inputfile, string outputfile, string keyfile)
+{
+    int mode = 3;
+    this->mode = mode;
+
+    this->inputfile = inputfile;
+    finputfile.open(inputfile, fstream::in | fstream::out | fstream::app);
+    this->outputfile = outputfile;
+    finputfile.open(outputfile, fstream::in | fstream::out | fstream::app);
+    this->keyfile = keyfile;
+    fkeyfile.open(keyfile, fstream::in | fstream::out | fstream::app);
+    chekfile();
+}
+
 void VigenereCipher::setkey(string key) {
     transform(key.begin(), key.end(), key.begin(), [](unsigned char c) {return toupper(c); });
     this->key = key;
@@ -9,7 +53,7 @@ void VigenereCipher::setkey(string key) {
 
 string VigenereCipher::encrypt(const string& text) {
     string output;
-    //øèôğîâàíèå òåêñòà
+    //ÑˆĞ¸Ñ„Ñ€Ğ¾Ğ²Ğ°Ğ½Ğ¸Ğµ Ñ‚ĞµĞºÑÑ‚Ğ°
     string effectiveKey = getKey(text, key);
     for (size_t i = 0; i < text.size(); i++) {
         output += cipherChar(text[i], effectiveKey[i], true);
@@ -19,7 +63,7 @@ string VigenereCipher::encrypt(const string& text) {
 
 string VigenereCipher::decrypt(const string& text) {
     string output;
-    //Äåøèôğîâàíèå òåêñòà
+    //Ğ”ĞµÑˆĞ¸Ñ„Ñ€Ğ¾Ğ²Ğ°Ğ½Ğ¸Ğµ Ñ‚ĞµĞºÑÑ‚Ğ°
     string effectiveKey = getKey(text, key);
     for (size_t i = 0; i < text.size(); i++) {
         output += cipherChar(text[i], effectiveKey[i], false);
@@ -27,19 +71,122 @@ string VigenereCipher::decrypt(const string& text) {
     return output;
 }
 
+VigenereCipher::~VigenereCipher()
+{
+
+    switch (mode) {
+    case 1:
+        finputfile.close();
+    case 2:
+        finputfile.close();
+        foutputfile.close();
+    case 3:
+        finputfile.close();
+        foutputfile.close();
+        fkeyfile.close();
+
+
+    }
+
+}
+
+int VigenereCipher::read_Datafile(int cryptomode, string key)
+{
+    switch (mode)
+    {
+    case 1:
+        if (cryptomode == 0) {
+            std::vector<char> buffer(std::istreambuf_iterator<char>(finputfile), (std::istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            setkey(key);
+            encrypt(str);
+            finputfile.close();
+            deleteFile(inputfile);
+            finputfile.open(inputfile, fstream::in | fstream::out | fstream::app);
+            finputfile << str;
+            return 0;
+        }
+        else if (cryptomode == 1) {
+            std::vector<char> buffer(std::istreambuf_iterator<char>(finputfile), (std::istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            setkey(key);
+            decrypt(str);
+            finputfile.close();
+            deleteFile(inputfile);
+            finputfile.open(inputfile, fstream::in | fstream::out | fstream::app);
+            finputfile << str;
+            return 0;
+        }
+        break;
+    case 2:
+        if (cryptomode == 0) {
+            std::vector<char> buffer(std::istreambuf_iterator<char>(finputfile), (std::istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            setkey(key);
+            encrypt(str);
+            foutputfile << str;
+
+        }
+        else if (cryptomode == 1) {
+            std::vector<char> buffer(std::istreambuf_iterator<char>(finputfile), (std::istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            setkey(key);
+            decrypt(str);
+            finputfile << str;
+        }
+        break;
+    case 3:
+        cerr << "Ğ¾ÑˆĞ¸Ğ±ĞºĞ° ÑĞ»Ğ¸ÑˆĞºĞ¾Ğ¼ Ğ¼Ğ½Ğ¾Ğ³Ğ¾ Ğ°Ñ€Ğ³ÑƒĞ¼ĞµĞ½Ñ‚Ğ¾Ğ² Ğ² Ñ„ÑƒĞ½ĞºÑ†Ğ¸Ğ¸!\n";
+        break;
+    }
+}
+
+int VigenereCipher::read_Datafile(int cryptomode)
+{
+    if (mode == 3) {
+        if (cryptomode == 0) {
+            vector<char> buffer(istreambuf_iterator<char>(finputfile), (istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            vector<char> keybuffer(istreambuf_iterator<char>(fkeyfile), (istreambuf_iterator<char>()));
+            string keystr(keybuffer.begin(), buffer.end());
+            setkey(keystr);
+            encrypt(str);
+            foutputfile << str;
+            return 0;
+        }
+        else if (cryptomode == 1) {
+            vector<char> buffer(istreambuf_iterator<char>(finputfile), (istreambuf_iterator<char>()));
+            string str(buffer.begin(), buffer.end());
+            vector<char> keybuffer(istreambuf_iterator<char>(fkeyfile), (istreambuf_iterator<char>()));
+            string keystr(keybuffer.begin(), keybuffer.end());
+            setkey(keystr);
+            decrypt(str);
+            foutputfile << str;
+            return 0;
+        }
+    }
+    else {
+
+
+        cerr << "Ğ¾ÑˆĞ¸Ğ±ĞºĞ° ÑĞ»Ğ¸ÑˆĞºĞ¾Ğ¼ Ğ¼Ğ°Ğ»Ğ¾ Ğ°Ñ€Ğ³ÑƒĞ¼ĞµĞ½Ñ‚Ğ¾Ğ² Ğ² Ñ„ÑƒĞ½ĞºÑ†Ğ¸Ğ¸!\n";
+        return 1;
+    }
+
+}
+
 string VigenereCipher::getKey(const string& text, const string& key) {
-    //Ïîëó÷àòü êëş÷
+    //ĞŸĞ¾Ğ»ÑƒÑ‡Ğ°Ñ‚ÑŒ ĞºĞ»ÑÑ‡
     string resultKey = key;
     while (resultKey.size() < text.size()) {
         resultKey += key;
     }
-    //Îáğåçàåì êëş÷
+    //ĞĞ±Ñ€ĞµĞ·Ğ°ĞµĞ¼ ĞºĞ»ÑÑ‡
     resultKey = resultKey.substr(0, text.size());
     return resultKey;
 }
 
 char VigenereCipher::cipherChar(char c, char key, bool encode) {
-    //Øèôğîâàíèå/Äåøèôğîâàíèå ñèìâîëà
+    //Ğ¨Ğ¸Ñ„Ñ€Ğ¾Ğ²Ğ°Ğ½Ğ¸Ğµ/Ğ”ĞµÑˆĞ¸Ñ„Ñ€Ğ¾Ğ²Ğ°Ğ½Ğ¸Ğµ ÑĞ¸Ğ¼Ğ²Ğ¾Ğ»Ğ°
 
     if (!isalpha(c)) return c;
 
@@ -48,12 +195,89 @@ char VigenereCipher::cipherChar(char c, char key, bool encode) {
     int offset = key - keyBase;
 
     if (encode) {
-        //Øèôğîâàíèå ñèìâîëà
+        //Ğ¨Ğ¸Ñ„Ñ€Ğ¾Ğ²Ğ°Ğ½Ğ¸Ğµ ÑĞ¸Ğ¼Ğ²Ğ¾Ğ»Ğ°
         return base + (c - base + offset) % 26;
     }
     else {
-        //Äåøèôâğîâàíèå ñèìâîëà
+        //Ğ”ĞµÑˆĞ¸Ñ„Ğ²Ñ€Ğ¾Ğ²Ğ°Ğ½Ğ¸Ğµ ÑĞ¸Ğ¼Ğ²Ğ¾Ğ»Ğ°
         return base + (26 + c - base + offset) % 26;
-        // òóò íóæíî äîáàâèòü ïğîâåğêó íà äèàïàçîí
+        // Ñ‚ÑƒÑ‚ Ğ½ÑƒĞ¶Ğ½Ğ¾ Ğ´Ğ¾Ğ±Ğ°Ğ²Ğ¸Ñ‚ÑŒ Ğ¿Ñ€Ğ¾Ğ²ĞµÑ€ĞºÑƒ Ğ½Ğ° Ğ´Ğ¸Ğ°Ğ¿Ğ°Ğ·Ğ¾Ğ½
     }
+}
+
+bool VigenereCipher::deleteFile(string filePath)
+{
+    if (remove(filePath.c_str()) != 0) {
+        cerr << "Error deleting file: " << filePath << "\n";
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+int VigenereCipher::chekfile()
+{
+    switch (mode)
+    {
+    case 1:
+
+        if (finputfile.is_open()) {
+
+            return 0;
+        }
+        else
+        {
+            cerr << "error opened file\n";
+            return 1;
+        }
+    case 2:
+
+        if (finputfile.is_open()) {
+            if (foutputfile.is_open()) {
+
+                return 0;
+            }
+            else
+            {
+                cerr << "error opened file\n";
+                return 2;
+            }
+
+        }
+        else
+        {
+            cerr << "error opened file\n";
+            return 1;
+        }
+    case 3:
+        if (finputfile.is_open()) {
+            if (foutputfile.is_open()) {
+
+                if (fkeyfile.is_open()) {
+
+                    return 0;
+
+                }
+                else
+                {
+                    cerr << "error opened file\n";
+                    return 3;
+                }
+            }
+            else
+            {
+                cerr << "error opened file\n";
+                return 2;
+            }
+
+        }
+        else
+        {
+            cerr << "error opened file\n";
+            return 1;
+        }
+
+    }
+
 }
