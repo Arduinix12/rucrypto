c1b328d9835e992fe43b06f07e14fa4e8b053829
Arduinix12 <151068595+Arduinix12@users.noreply.github.com>
2024-07-25 11:10:28 +0300

    Update Caesar.h

--- Caesar.h
+++ Caesar.h
@@ -1,23 +1,48 @@
 #ifndef CAESER_H
 #define CAESER_H
 
+#include<fstream>
 #include <cctype>
 #include <iostream>
 #include <algorithm>
 #include <string>
 #include <typeinfo>
-
+#include<vector>
+#include<cstdio>
+#include <stdlib.h>
 using namespace std;
 
 class Caeser {
 public:
+	Caeser();
+	Caeser(string filename);
+	Caeser(string inputfile,string outputfile);
+	Caeser(string inputfile, string outputfile, string keyfile);
+	~Caeser();
 	string decript(string str, int key);
 	string encript(string str, int key);
-	string getword(string str);
-	int getkey(int key);
+	string setword(string str);
+	int setkey(int key);
+	int read_Datafile( int cryptomode,int key);
+	int read_Datafile(int cryptomode);
+	
+
 private:
+	fstream finputfile;
+	fstream foutputfile;
+	fstream fkeyfile;
 	string str;
-	int key;
+		string	inputfile = "con";
+		string outputfile = "con";
+		string keyfile = "con";
+		string errorstr = "con";
+		string end = ".txt";
+		int key;
+		int mode;
+		int cryptomode;
 	int chekkey(int key);
+	bool deleteFile(string filePath);
+	int chekfile();
+
 };
 #endif
